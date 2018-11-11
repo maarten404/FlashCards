d0b391a1f3a3053de5cb78db9e050ef38627020b
mfugers <maartenfugers@gmail.com>
2018-11-11 15:56:22 +0100

    Login/logout/register

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 """Define URLs for users"""
 
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 
@@ -12,7 +12,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), 
         name='login'),
     #logout page
-    #path('logout/', views.logout_view, name='logout'),
+    path('logout/', views.logout_view, name='logout'),
     #Registration page
-    #path('register/', views.register, name='register'),
+    path('register/', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse ("flashcards:index"))
+
+def register(request):
+    """Register a new user."""
+    if request.method != 'POST':
+        # Display blank registration from.
+        form = UserCreationForm()
+    else:
+        #Process completed form
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Log the user in and then redirect to index
+            authenticated_user = authenticate(username=new_user.username,
+                password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('flashcards:index'))
+
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
